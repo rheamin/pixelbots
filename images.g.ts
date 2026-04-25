858183029a0f2154f0783d7e0ed3acfdcae6c441
Rhea <technogal@pm.me>
2026-04-24 21:58:32 -0600

    added assets and dependencies

---
+++ images.g.ts
@@ -0,0 +1,191 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "hammerBot0":return img`
+. . . . . . . . . . . 6 . . . . 
+. . . . . . . . . f 6 6 6 . . . 
+. . . . . . . . f f f 6 6 6 . . 
+. . . . . . . . f f f f 6 6 6 . 
+. . . . . . . f f f 4 4 f 6 . . 
+. . . . . . . . f f 4 4 4 f f . 
+. . . . . . . . f f f 4 4 4 f . 
+. . . . f . . 6 6 f f f 4 f . . 
+. . f f f f f 6 6 6 f f f . . . 
+. f f f f f f f 6 6 6 f . . . . 
+. 6 f f 4 4 f f f 6 6 2 . . . . 
+6 6 6 f 4 4 4 f f f 2 2 2 2 . . 
+. 6 6 6 f 4 4 4 f . . 2 2 2 . . 
+. . 6 6 6 f 4 f . . . 2 2 2 . . 
+. . . 6 . f f . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "hammerBot1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. f f f . . . . . . . . f f f . 
+6 6 f f f f . . . . f f f f 6 6 
+6 6 f 4 f f f f f f f f 4 f 6 6 
+6 6 f 4 4 f f 6 6 f f 4 4 f 6 6 
+6 6 f 4 4 f f 6 6 f f 4 4 f 6 6 
+. f f 4 4 f f 6 6 f f 4 4 f f . 
+. . f f f f f 6 6 f f f f f . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+`;
+            case "image3":
+            case "hammerBot2":return img`
+. . . . 6 . . . . . . . . . . . 
+. . . 6 6 6 f . . . . . . . . . 
+. . 6 6 6 f f f . . . . . . . . 
+. 6 6 6 f f f f . . . . . . . . 
+. . 6 f 4 4 f f f . . . . . . . 
+. f f 4 4 4 f f . . . . . . . . 
+. f 4 4 4 f f f . . . . . . . . 
+. . f 4 f f f 6 6 . . f . . . . 
+. . . f f f 6 6 6 f f f f f . . 
+. . . . f 6 6 6 f f f f f f f . 
+. . . . 2 6 6 f f f 4 4 f f 6 . 
+. . 2 2 2 2 f f f 4 4 4 f 6 6 6 
+. . 2 2 2 . . f 4 4 4 f 6 6 6 . 
+. . 2 2 2 . . . f 4 f 6 6 6 . . 
+. . . . . . . . . f f . 6 . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "hammerBot3":return img`
+. . . . . . 6 6 6 6 . . . . . . 
+. . . . . f 6 6 6 6 f . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . f f 4 4 4 4 f . . . . 
+. . . . . . f f 4 4 4 f . . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f f f f f . 2 2 . 
+. . . . . . . f 6 6 6 6 2 2 2 2 
+. . . . . . . f 6 6 6 6 2 2 2 2 
+. . . . . . . f f f f f . 2 2 . 
+. . . . . . f f f f f f . . . . 
+. . . . . . f f 4 4 4 f . . . . 
+. . . . . f f 4 4 4 4 f . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . f 6 6 6 6 f . . . . . 
+. . . . . . 6 6 6 6 . . . . . . 
+`;
+            case "image5":
+            case "hammerBot4":return img`
+. . . . . . 6 6 6 6 . . . . . . 
+. . . . . f 6 6 6 6 f . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . f 4 4 4 4 f f . . . . . 
+. . . . f 4 4 4 f f . . . . . . 
+. . . . f f f f f f . . . . . . 
+. 2 2 . f f f f f . . . . . . . 
+2 2 2 2 6 6 6 6 f . . . . . . . 
+2 2 2 2 6 6 6 6 f . . . . . . . 
+. 2 2 . f f f f f . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . f 4 4 4 f f . . . . . . 
+. . . . f 4 4 4 4 f f . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . f 6 6 6 6 f . . . . . 
+. . . . . . 6 6 6 6 . . . . . . 
+`;
+            case "image6":
+            case "hammerBot5":return img`
+. . . . . . . . . . . . . . . . 
+. . . 6 . f f . . . . . . . . . 
+. . 6 6 6 f 4 f . . . 2 2 2 . . 
+. 6 6 6 f 4 4 4 f . . 2 2 2 . . 
+6 6 6 f 4 4 4 f f f 2 2 2 2 . . 
+. 6 f f 4 4 f f f 6 6 2 . . . . 
+. f f f f f f f 6 6 6 f . . . . 
+. . f f f f f 6 6 6 f f f . . . 
+. . . . f . . 6 6 f f f 4 f . . 
+. . . . . . . . f f f 4 4 4 f . 
+. . . . . . . . f f 4 4 4 f f . 
+. . . . . . . f f f 4 4 f 6 . . 
+. . . . . . . . f f f f 6 6 6 . 
+. . . . . . . . f f f 6 6 6 . . 
+. . . . . . . . . f 6 6 6 . . . 
+. . . . . . . . . . . 6 . . . . 
+`;
+            case "image7":
+            case "hammerBot6":return img`
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . f f f f f 6 6 f f f f f . . 
+. f f 4 4 f f 6 6 f f 4 4 f f . 
+6 6 f 4 4 f f 6 6 f f 4 4 f 6 6 
+6 6 f 4 4 f f 6 6 f f 4 4 f 6 6 
+6 6 f 4 f f f f f f f f 4 f 6 6 
+6 6 f f f f . . . . f f f f 6 6 
+. f f f . . . . . . . . f f f . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image8":
+            case "hammerBot7":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . f f . 6 . . . 
+. . 2 2 2 . . . f 4 f 6 6 6 . . 
+. . 2 2 2 . . f 4 4 4 f 6 6 6 . 
+. . 2 2 2 2 f f f 4 4 4 f 6 6 6 
+. . . . 2 6 6 f f f 4 4 f f 6 . 
+. . . . f 6 6 6 f f f f f f f . 
+. . . f f f 6 6 6 f f f f f . . 
+. . f 4 f f f 6 6 . . f . . . . 
+. f 4 4 4 f f f . . . . . . . . 
+. f f 4 4 4 f f . . . . . . . . 
+. . 6 f 4 4 f f f . . . . . . . 
+. 6 6 6 f f f f . . . . . . . . 
+. . 6 6 6 f f f . . . . . . . . 
+. . . 6 6 6 f . . . . . . . . . 
+. . . . 6 . . . . . . . . . . . 
+`;
+            case "image9":
+            case "hammerHitbox":return img`
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+2 2 2 2 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
 
+scene.setBackgroundColor(1)
+
+/**
+ *  0  1  2
+ *  3 [ ] 4
+ *  5  6  7
+ */
+
+let player1 = sprites.create(assets.image`hammerBot6`, SpriteKind.Player)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), player1)
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
+
